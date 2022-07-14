389410814181d433a6664291b145830cdef302fc
genkinaoko <surtaka944@gmail.com>
2022-07-14 11:33:07 +0900

    added by takasaki

---
+++ First_Project/Assets/Scripts/Spectrum.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class Spectrum : MonoBehaviour
+{   
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 空の Audio Sourceを取得
+        var audio = GetComponent<AudioSource>();
+        if ((audio != null)&&(Microphone.devices.Length>0)) // オーディオソースとマイクがある
+        {
+            string devName = Microphone.devices[0]; // 複数見つかってもとりあえず0番目のマイクを使用
+            int minFreq, maxFreq;
+            Microphone.GetDeviceCaps(devName, out minFreq, out maxFreq); // 最大最小サンプリング数を得る
+            audio.clip = Microphone.Start(devName, true, 1, minFreq); // 音の大きさを取るだけなので最小サンプリングで十分
+            audio.Play(); //マイクをオーディオソースとして実行(Play)開始
+        }
+
+        // Audio Source の Audio Clip をマイク入力に設定
+        // 引数は、デバイス名（null ならデフォルト）、ループ、何秒取るか、サンプリング周波数
+        //audio.clip = Microphone.Start(null, false, 10, 44100);
+        // マイクが Ready になるまで待機（一瞬）
+        //while (Microphone.GetPosition(null) <= 0) {}
+        // 再生開始（録った先から再生、スピーカーから出力するとハウリングします）
+        //audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var spectrum = GetComponent<AudioSource>().GetSpectrumData(1024, 0, FFTWindow.Hamming);
+        for (int i = 1; i < spectrum.Length - 1; ++i) {
+            Debug.DrawLine(
+                    new Vector3(i - 1, spectrum[i] + 10, 0), 
+                    new Vector3(i, spectrum[i + 1] + 10, 0), 
+                    Color.red);
+            Debug.DrawLine(
+                    new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), 
+                    new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), 
+                    Color.cyan);
+            Debug.DrawLine(
+                    new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), 
+                    new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), 
+                    Color.green);
+            Debug.DrawLine(
+                    new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), 
+                    new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), 
+                    Color.yellow);
+    }
+    }
+}
---
+++ First_Project/Assets/Scripts/Volume.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Volume : MonoBehaviour {
+    [SerializeField, Range(0f, 10f)] float m_gain = 1f; // 音量に掛ける倍率
+    float m_volumeRate; // 音量(0-1)
+    // Use this for initialization
+    void Start () {
+        AudioSource aud = GetComponent<AudioSource>();
+        if ((aud != null)&&(Microphone.devices.Length>0)) // オーディオソースとマイクがある
+        {
+            string devName = Microphone.devices[0]; // 複数見つかってもとりあえず0番目のマイクを使用
+            int minFreq, maxFreq;
+            Microphone.GetDeviceCaps(devName, out minFreq, out maxFreq); // 最大最小サンプリング数を得る
+            aud.clip = Microphone.Start(devName, true, 2, minFreq); // 音の大きさを取るだけなので最小サンプリングで十分
+            aud.Play(); //マイクをオーディオソースとして実行(Play)開始
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Debug.Log(m_volumeRate);
+    }
+
+    // オーディオが読まれるたびに実行される
+    private void OnAudioFilterRead(float[] data, int channels)
+    {
+        float sum = 0f;
+        for (int i = 0; i < data.Length; i++)
+        {
+            sum += Mathf.Pow(data[i], 2.0f);
+        }
+        sum /= data.Length;
+
+        m_volumeRate = 10.0f * Mathf.Log10(sum);
+    }
+}
