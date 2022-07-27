0141134c2701d36c2d1404def7475cdfe4f48650
genkinaoko <surtaka944@gmail.com>
2022-07-27 11:08:34 +0900

    takasaki edited

--- First_Project/Assets/Scripts/Spectrum.cs
+++ First_Project/Assets/Scripts/Spectrum.cs
@@ -15,7 +15,7 @@ public class Spectrum : MonoBehaviour
             string devName = Microphone.devices[0]; // 複数見つかってもとりあえず0番目のマイクを使用
             int minFreq, maxFreq;
             Microphone.GetDeviceCaps(devName, out minFreq, out maxFreq); // 最大最小サンプリング数を得る
-            audio.clip = Microphone.Start(devName, true, 50, 72000); // 音の大きさを取るだけなので最小サンプリングで十分
+            audio.clip = Microphone.Start(devName, true, 10, 44100); // 音の大きさを取るだけなので最小サンプリングで十分
             audio.Play(); //マイクをオーディオソースとして実行(Play)開始
         }
 
@@ -32,7 +32,7 @@ public class Spectrum : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var spectrum = GetComponent<AudioSource>().GetSpectrumData(1024, 0, FFTWindow.BlackmanHarris);
+        var spectrum = GetComponent<AudioSource>().GetSpectrumData(2048, 0, FFTWindow.BlackmanHarris);
         var bun = "";
         for (int i = 1; i < spectrum.Length - 1; ++i) {
             Debug.DrawLine(
@@ -42,7 +42,7 @@ public class Spectrum : MonoBehaviour
             Debug.DrawLine(
                     new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), 
                     new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), 
-                    Color.cyan);
+                    Color.black);
 
             Debug.DrawLine(
                     new Vector3(Mathf.Log(i - 1), spectrum[i - 1] + 10, 1), 
